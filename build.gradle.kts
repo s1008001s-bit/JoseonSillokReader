85cfd0ffe15bbc1d0a79e9ea9ca2bc852600ab1e
s1008001s-bit <s1008001s@gmail.com>
2026-09-01 11:06:51 +0900

    app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace="kr.co.personal.sillokreader"; compileSdk=35
+ defaultConfig { applicationId="kr.co.personal.sillokreader"; minSdk=26; targetSdk=35; versionCode=3; versionName="2.0.0" }
+ buildTypes { release { isMinifyEnabled=false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro") }; debug { applicationIdSuffix=".debug" } }
+ compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }; kotlinOptions { jvmTarget="17" }
 }
+dependencies { implementation("androidx.core:core-ktx:1.15.0"); implementation("androidx.appcompat:appcompat:1.7.0") }
